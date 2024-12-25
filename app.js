72d77ba37e94588c093c9c7c087c2c8652f6c683
Anton Liu <liuantonliu@gmail.com>
2024-12-24 21:36:49 -0500

    add particle back

--- app.js
+++ app.js
@@ -167,14 +167,3 @@ $('.box').slick({
     }
   ]
 });
-
-//mute everything
-function muteMe(elem) {
-    elem.muted = true;
-    elem.pause();
-}
-
-// Try to mute all video and audio elements on the page
-function mutePage() {
-    document.querySelectorAll("video, audio").forEach((elem) => muteMe(elem));
-}
